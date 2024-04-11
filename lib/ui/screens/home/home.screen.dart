35c982a0c0189e13eced14308f65c1acad3b59fb
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2024-04-11 10:18:27 +0200

    page home done

--- lib/ui/screens/home/home.screen.dart
+++ lib/ui/screens/home/home.screen.dart
@@ -19,6 +19,7 @@ class _HomeScreeenState extends ConsumerState<HomeScreen> {
     
     final HomeState state = ref.watch(homeProvider);
     List<ResultEntity>? list = state.placeEntity!.results;
+    final HomeViewModel viewModel = ref.read(homeProvider.notifier);
 
     return state.loading
         ? const Center(child: CircularProgressIndicator())
@@ -26,7 +27,35 @@ class _HomeScreeenState extends ConsumerState<HomeScreen> {
             itemCount: state.placeEntity!.results!.length,
             itemBuilder: (BuildContext context, int index) {
               return ListTile(
-                
+                title: Card(
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        height: 100,
+                        child:Image(image: NetworkImage(list![index].photo?.url??'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'), fit: BoxFit.cover,),),
+                        const Padding(padding: EdgeInsets.all(8.0)),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              list[index].appellationCourante!,
+                              style: const TextStyle(
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  viewModel.showDetailDialog(context, list[index]);
+                },
               );
             },
           );
--- lib/ui/screens/home/home.viewmodel.dart
+++ lib/ui/screens/home/home.viewmodel.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:template_flutter_but/application/injections/initializer.dart';
 import 'package:template_flutter_but/domain/entities/place.entity.dart';
+import 'package:template_flutter_but/domain/entities/result.entity.dart';
 import 'package:template_flutter_but/domain/repository/places.repository.dart';
 import 'package:template_flutter_but/foundation/client/dio.client.dart';
 import 'package:template_flutter_but/ui/abstraction/view_model_abs.dart';
@@ -35,12 +38,36 @@ class HomeViewModel extends ViewModelAbs<HomeViewModel, HomeState> {
     getPlaces();
   }
 
-
   void getPlaces() async {
     updateLoading(true);
     final PlaceEntity placeEntity = await _placesRepository.getPlaces();
     updateLoading(false);
     updatePlaceModel(placeEntity);
-    
+  }
+
+  Future<void> showDetailDialog(BuildContext context, ResultEntity data) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(data.appellationCourante ?? 'No name'),
+            content: SingleChildScrollView(
+              child: Column(
+              children: <Widget>[
+                Row(
+                  children: [const Text("Protection : "), Text(data.niveauDeProtection ?? "No protection")],
+                ),
+                Image(
+                  image: NetworkImage(data.photo?.url ??
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png'),
+                  fit: BoxFit.cover,
+                ),
+                Text(data.appellationCourante ?? 'No name', style: const TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold)),
+                Text(data.adresseBanSig ?? 'No address', style: const TextStyle(fontSize: 10.0)),
+                Text(data.historique ?? 'No description' , style: const TextStyle(fontSize: 8.0),),
+              ],
+            ),
+          ));
+        });
   }
 }
